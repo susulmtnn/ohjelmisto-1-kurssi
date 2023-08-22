bc52710d7d266c2a2aa33e47fe39ebe93b512c47
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-22 12:55:59 +0300

    Exercise 4 completed

--- moduuli-02/example4.py
+++ moduuli-02/example4.py
@@ -1,10 +1,9 @@
 #Kirjoita ohjelma, joka kysyy kolme kokonaislukua.
 #Ohjelma tulostaa lukujen summan, tulon ja keskiarvon.
 listnum = list(input("Anna kolme kokonaislukua"))
-print(listnum)
 num1 = int(listnum[0])
 num2 = int(listnum[1])
 num3 = int(listnum[2])
-print("Summa=" + str((num1+num2+num3)) +"\n" +
-      "Tulo=" + str((num1*num2*num3)) + "\n" +
-      "Keskiarvo" + str(num1))
+print("Summa= " + str((num1+num2+num3)) +"\n" +
+      "Tulo= " + str((num1*num2*num3)) + "\n" +
+      "Keskiarvo= " + str((round((num1+num2+num3)/3, 2))))
