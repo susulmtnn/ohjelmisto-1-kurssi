4fae4958cb3729966a10038cc62a6b7d104e5b88
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-22 18:45:29 +0300

    Example 6 with list idea in it but commented out

---
+++ moduuli-02/example6.py
@@ -0,0 +1,32 @@
+'''Kirjoita ohjelma, joka arpoo ja tulostaa kaksi
+ erilaista numerolukon koodia:
+kolmenumeroisen koodin, jonka kukin numeromerkki on väliltä 0..9.
+nelinumeroisen koodin, jonka kukin numeromerkki on väliltä 1..6.
+Vihje: tutustu random.randint()-funktion käyttöön.
+'''
+
+import random
+i=0
+a=0
+lista=[]
+str1=""
+str2=""
+lista2=[]
+while i<=2:
+    ekakoodi = random.randint(0,9)
+    str(ekakoodi)
+    #lista.append(ekakoodi)
+    str1+=str(ekakoodi) + ", "
+    i+=1
+while a<=3:
+    tokakoodi = random.randint(1, 6)
+    str(tokakoodi)
+    #lista2.append(tokakoodi)
+    str2+=str(tokakoodi) + ", "
+    a+= 1
+str(lista2)
+#print(lista2)
+print(str1)
+print(str2)
+#str(lista)
+#print(lista)
