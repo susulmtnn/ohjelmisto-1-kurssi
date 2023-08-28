df73597e4db74c5ab6da0d5ee04fb1297828171b
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-28 15:37:42 +0300

    Example 1 module 3 completed

---
+++ moduuli-03/example3.2.py
@@ -0,0 +1,20 @@
+'''Kirjoita ohjelma, joka kysyy käyttäjältä laivan hyttiluokan (LUX, A, B, C) ja
+ tulostaa sen sanallisen kuvauksen alla olevan luettelon mukaisesti.
+ Tehtävässä on käytettävä if/elif/else-toistorakennetta.
+
+LUX on parvekkeellinen hytti yläkannella.
+A on ikkunallinen hytti autokannen yläpuolella.
+B on ikkunaton hytti autokannen yläpuolella.
+C on ikkunaton hytti autokannen alapuolella.
+Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.'''
+
+hytclas = input("Syötä hyttiluokka")
+
+if hytclas == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella.")
+elif hytclas == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+elif hytclas == "C":
+    print("B on ikkunaton hytti autokannen yläpuolella.")
+else:
+    print("Virheellinen hyttiluokka")
