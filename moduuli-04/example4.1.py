879e6fd0cdb735b3c5253db164a36c253bedbebf
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-29 11:14:58 +0300

    Example 1 module 4 completed

---
+++ moduuli-04/example4.1.py
@@ -0,0 +1,12 @@
+'''Kirjoita while-toistorakennetta käyttävä ohjelma,
+joka tulostaa kolmella jaolliset luvut väliltä 1..1000.'''
+
+a=1
+while 1<=a<=1000:
+    while a<=1000:
+        while a%3==0:
+            print(a)
+            a+= 1
+        while a%3!=0:
+            a+= 1
+print("Kaikki kolmella jaolliset tulostettu")
