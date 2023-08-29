72768124d58d06d9632613d7e6c72e1e0445ecdf
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-29 15:07:57 +0300

    Example 3 of module 4 fixed a bit

--- moduuli-04/example4.3.py
+++ moduuli-04/example4.3.py
@@ -1,21 +1,17 @@
-'''Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
-kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
-Lopuksi ohjelma tulostaa saaduista luvuista pienimmän ja suurimman.'''
-
-unum1= input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+num1= input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
 num2 = float(num1)
 num3= float(num1)
 
 while num1 != " ":
     num1 =float(num1)
+    while num1 == " ":
+        print("pienin luku:" + num1 + "ja suurin luku:" + num2)
+        num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
     while num1 <= num2:
         num2 = num1
         num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
     while num1 >= num3:
         num3 = num1
         num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-    else:
+    while num2<=num1<=num3:
          num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-while user_output == " ":
-        print("pienin luku:" + num1 + "ja suurin luku:" + num2)
-        num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
