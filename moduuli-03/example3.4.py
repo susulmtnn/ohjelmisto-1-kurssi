ca52ffdb0df3f60084aa5b6ebf4c21a065c3cfe0
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-29 09:52:26 +0300

    Example 4 module 3 completed

---
+++ moduuli-03/example3.4.py
@@ -0,0 +1,13 @@
+'''Kirjoita ohjelma, joka kysyy vuosiluvun ja ilmoittaa, onko annettu vuosi karkausvuosi. Vuosi on karkausvuosi, jos se on jaollinen neljällä.
+ Sadalla jaolliset vuodet ovat karkausvuosia vain jos ne ovat jaollisia
+ myös neljälläsadalla.'''
+
+user_input = int(input("Anna vuosiluku"))
+
+if user_input%100 != 0 and user_input%4 == 0:
+    print("karkausvuosi")
+elif user_input%4 == 0 and user_input%400==0:
+    #if user_input%400==0:
+    print("karkausvuosi")
+else:
+    print("ei karkausvuosi")
