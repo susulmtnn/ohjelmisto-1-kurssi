a7d604660fd2c78b876a61d2949324e543bff203
susulmtnn <suvi.liimatainen@gmail.com>
2023-08-29 15:42:40 +0300

    Example 3 of module 4 completed

--- moduuli-04/example4.3.py
+++ moduuli-04/example4.3.py
@@ -6,13 +6,18 @@ while num1 != " ":
     num1 =float(num1)
     while num1 == " ":
         print("pienin luku:" + num1 + "ja suurin luku:" + num2)
-        num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-    while num1 <= num2:
+        num1 = input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+    while type(num1)!= str and num1 <= num2:
+        num1 = float(num1)
         num2 = num1
-        num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-    while num1 >= num3:
+        num1 = input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+    while type(num1)!= str and num1 >= num3:
+        num1 = float(num1)
         num3 = num1
-        num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-    while num2<=num1<=num3:
-         num1 = float(input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki"))
-
+        num1 = input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+    while type(num1)!= str and num2<=num1<=num3:
+         num1 = input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+while num1 == " ":
+    print("pienin luku: " + str(num2) + "ja suurin luku: " + str(num3))
+    num1 = input("anna luku, mikäli haluat lopettaa, anna tyhjä lopetusmerkki")
+    break
