caef6d602a8a8450c1cc6da20b9b9ff22ea65673
susulmtnn <suvi.liimatainen@gmail.com>
2023-09-04 14:38:38 +0300

    One while loop was too much line 6 and 7. Removed the one on line 6

--- moduuli-04/example4.1.py
+++ moduuli-04/example4.1.py
@@ -3,10 +3,9 @@ joka tulostaa kolmella jaolliset luvut väliltä 1..1000.'''
 
 a=1
 while 1<=a<=1000:
-    while a<=1000:
-        while a%3==0:
-            print(a)
-            a+= 1
-        while a%3!=0:
-            a+= 1
+    while a%3==0:
+        print(a)
+        a+= 1
+    while a%3!=0:
+        a+= 1
 print("Kaikki kolmella jaolliset tulostettu")
