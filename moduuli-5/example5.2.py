e882014bce000453f34eb8e98eab3a9c205459b9
susulmtnn <suvi.liimatainen@gmail.com>
2023-09-05 10:14:09 +0300

    example 5.2 completed

---
+++ moduuli-5/example5.2.py
@@ -0,0 +1,16 @@
+'''Kirjoita ohjelma, joka kysyy käyttäjältä lukuja siihen saakka,
+kunnes tämä syöttää tyhjän merkkijonon lopetusmerkiksi.
+Lopuksi ohjelma tulostaa saaduista luvuista viisi suurinta suuruusjärjestyksessä suurimmasta alkaen.
+Vihje: listan alkioiden lajittelujärjestyksen voi kääntää antamalla sort-metodille
+argumentiksi reverse=True.'''
+
+
+user_input = input("Anna lukuja. Jos haluat tulostaa viisi suurinta, anna tyhjä")
+
+lista = []
+
+while user_input != " ":
+    lista.append(int(user_input))
+    user_input = input("Anna lukuja. Jos haluat tulostaa viisi suurinta, anna tyhjä")
+lista.sort(reverse=True)
+print(lista[0:5])
