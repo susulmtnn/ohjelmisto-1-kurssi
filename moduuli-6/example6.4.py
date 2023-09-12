9b070a66da39abef7210d1ef80fc99a6437a7535
susulmtnn <suvi.liimatainen@gmail.com>
2023-09-12 09:18:06 +0300

    example 4 completed

---
+++ moduuli-6/example6.4.py
@@ -0,0 +1,15 @@
+'''Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja. Ohjelma
+ palauttaa listassa olevien lukujen summan. Kirjoita testausta varten pääohjelma,
+jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.'''
+
+import random
+
+def fc_sum_it(lista):
+    return sum(lista)
+
+def main():
+    lista= random.sample(range(10, 30), 5)
+    print(lista)
+    print(fc_sum_it(lista))
+
+main()
