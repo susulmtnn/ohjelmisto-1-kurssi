700ae213bc1e90da418e06ef206ae2e257609bd3
susulmtnn <suvi.liimatainen@gmail.com>
2023-11-09 12:27:52 +0200

    example 2 corrected. Fetchone gave me wrong result, fetchall fixes this

--- moduuli-13/example2.py
+++ moduuli-13/example2.py
@@ -1,36 +1,54 @@
-'''Toteuta taustapalvelu, joka palauttaa annettua lentokentän
-ICAO-koodia vastaavan lentokentän nimen ja kaupungin JSON-muodossa.
-Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta. Esimerkiksi
-EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
- Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport",
- "Municipality":"Helsinki"}.'''
-
-import mysql.connector
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import json
-
-
-connection = mysql.connector.connect(
-    host='127.0.0.1',
-    port=3306,
-    database='flight_game',
-    user= 'suvi',
-    password = 'HarmaaPoyta123',
-    autocommit = True
-    )
+import tietokantahaku
 
 app = Flask(__name__)
+app.debug = True
+# print(__name__)
 
-@app.route('/kenttä/<icao>')
-def get_ICAO(icao):
-    cursor = connection.cursor()
-    sql = f"SELECT ident, name, municipality from airport where ident= '{icao}'"
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    if result:
-        return result
-    else:
-        return (f"koodia {country_code} ei löytynyt, yritä uudelleen.")
+@app.route('/')
+def get_root():
+    return "Moro maailma!"
+
+
+@app.route('/kentta/<icao>')
+def multiply(icao):
+
+    try:
+        kentta, kaupunki = tietokantahaku.get_ICAO(icao)
+        status_code = 200
+        response_data = {
+            "ICAO": icao,
+            "Name": kentta,
+            "Municipality": kaupunki
+        }
+
+    except ValueError:
+        response_data = {
+            "msg": "Virheellinen icao-koodi",
+            "input_icao": icao
+        }
+        status_code = 400
+
+
+    # convert python dict to json format "manually"
+    response_data = json.dumps(response_data)
+    # setting up a status code for response needs Response object to be created
+    response = Response(response=response_data, status=status_code, mimetype="application/json")
+    return response
+
+# https://flask.palletsprojects.com/en/2.3.x/errorhandling/
+
+@app.errorhandler(404)
+def page_not_found(virhekoodi):
+    vastaus = {
+        "status" : "404",
+        "teksti" : "Virheellinen päätepiste"
+    }
+    jsonvast = json.dumps(vastaus)
+    # return Response(response=jsonvast, status=404, mimetype="application/json")
+    return render_template('404.html'), 404
+
 
 if __name__ == '__main__':
-    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
---
+++ moduuli-13/tietokantahaku.py
@@ -0,0 +1,33 @@
+'''Toteuta taustapalvelu, joka palauttaa annettua lentokentän
+ICAO-koodia vastaavan lentokentän nimen ja kaupungin JSON-muodossa.
+Tiedot haetaan opintojaksolla käytetystä lentokenttätietokannasta. Esimerkiksi
+EFHK-koodia vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/kenttä/EFHK.
+ Vastauksen on oltava muodossa: {"ICAO":"EFHK", "Name":"Helsinki Vantaa Airport",
+ "Municipality":"Helsinki"}.'''
+
+import mysql.connector
+from flask import Flask, request, Response
+
+
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user= 'suvi',
+    password = 'HarmaaPoyta123',
+    autocommit = True
+    )
+
+def get_ICAO(icao):
+    cursor = connection.cursor()
+    sql = f"SELECT name, municipality from airport where ident= '{icao}'"
+    cursor.execute(sql)
+    result = cursor.fetchone()
+    if result:
+        print(result)
+        print(type(result))
+        return result
+    else:
+        return 'not_found', 'not_found'
+
+
